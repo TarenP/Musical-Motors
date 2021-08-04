728029e1546b2b2106c9f720a1d8758b745734d0
Taren P <63258880+TarenP@users.noreply.github.com>
2021-08-03 23:02:32 -0700

    Web Scrapping works, Data is collected. Need to implement data into threads for chrods

--- music.py
+++ music.py
@@ -4,8 +4,14 @@ from music21 import *
 import re
 import pandas as pd
 import mido
-import pygame
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import shutil
+import csv
 
 filetype = int(input("1 for .mid filetype, 2 for .wav filetype "))
 
@@ -33,57 +39,71 @@ else:
     file_num = int(input("choose: "))
     file = files[file_num - 1]
     print(file)
+try:
+    os.remove("music.csv")
+except:
+    print("no csv file.")
+    
+f = open("music.csv", "a", newline="")
 
-
-
-
+#Upload newly generated midi file to a midi editor webpage and then create file that contains the contents of each row of the table
 driver = webdriver.Chrome('chromedriver.exe')
 driver.get('https://www.onlinemidi.com/Editor.php')
 
-
-
-
-
-keyboard_instruments = ["KeyboardInstrument", "Piano", "Harpsichord", "Clavichord", "Celesta", ]
-path = file
-def get_notes_chords_rests(instrument_type, path):
-    try:
-        midi = converter.parse(path)
-        parts = instrument.partitionByInstrument(midi)
-        note_list = []
-        for music_instrument in range(len(parts)):
-            if parts.parts[music_instrument].id in instrument_type:
-                for element_by_offset in stream.iterator.OffsetIterator(parts[music_instrument]):
-                    for entry in element_by_offset:
-                        if isinstance(entry, note.Note):
-                            note_list.append(str(entry.pitch))
-                        elif isinstance(entry, chord.Chord):
-                            note_list.append('.'.join(str(n) for n in entry.normalOrder))
-                        elif isinstance(entry, note.Rest):
-                            note_list.append('Rest')
-        return note_list
-    except Exception as e:
-        print("failed on ", path)
-        pass
-
- #create a function for taking parsing and extracting the notes
-def extract_notes(path):
-    mid = mido.MidiFile(path)
-    num = 0
+element = WebDriverWait(driver, 20).until(
+    EC.presence_of_element_located((By.ID, "idDivTitle"))
+)
+time.sleep(5)
+element = driver.find_element(By.XPATH, '//*[@id="filereader"]')
+element.send_keys("C:\\Users\\taren\Documents\\GitHub Repositories\\Musical-Motors\\" + file)
+time.sleep(2)
+mid = mido.MidiFile(file)
+num = 1
+try:
     for msg in mid.play():
-        if num > 0:
-            print(msg.note)
+        MBT = driver.find_element(By.XPATH, '//*[@id="idTbodyMidiData"]/tr['+ str(num) + ']/td[1]').text
+        M = (MBT.split(":")[0])
+        B = (MBT.split(":")[1])
+        T = (MBT.split(":")[2])
+        #print(MBT)
+        Pitch = driver.find_element(By.XPATH, '//*[@id="idTbodyMidiData"]/tr['+ str(num) + ']/td[2]').text
+        #print(Pitch)
+        Velocity = driver.find_element(By.XPATH, '//*[@id="idTbodyMidiData"]/tr['+ str(num) + ']/td[3]').text
+        #print(Velocity)
+        Duration = driver.find_element(By.XPATH, '//*[@id="idTbodyMidiData"]/tr['+ str(num) + ']/td[4]').text
+        Duration = (MBT.split(":")[0])
+        #print(Duration)
+        tup1 = (M)
+        tup2 = (B)
+        tup3 = (T)
+        tup4 = (Pitch)
+        tup5 = (Velocity)
+        tup6 = (Duration)
+        writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+        writer.writerow([tup1, tup2, tup3, tup4, tup5, tup6])
         num += 1
+except:
+    print("done filling out csv")
 
-#  #write to csv
-# def to_csv(notes_array):
-#      df = pd.DataFrame(notes_array, index=None, columns=None)
-#      df.to_csv("pirates.csv")
+web_tempo = driver.find_element(By.XPATH, '//*[@id="idTxtTempo"]')
+tempo = web_tempo.get_attribute("value")
+print(tempo)
+time.sleep(2)
 
- #using the functions
-notes_array = extract_notes(path)
-#print(notes_array)
 
-#notes = get_notes_chords_rests(keyboard_instruments, path)
-#print(notes)
+#used for downloading midi file, renaming it, and moving it into this folder from website
+
+#driver.find_element(By.XPATH, '//*[@id="idDivMain"]/table/tbody/tr/td[3]/div[1]/table/tbody/tr/td[2]/input').click()
+
+# prevName = 'D:\\taren\\Downloads\\OnlineMidi.mid'
+# newName = 'D:\\taren\\Downloads\\' + file
+# os.rename(prevName, newName)
+# os.remove('C:\\Users\\taren\\Documents\\GitHub Repositories\\Musical-Motors\\' + file)
+# shutil.move('D:\\taren\\Downloads\\' + file, 'C:\\Users\\taren\Documents\\GitHub Repositories\\Musical-Motors')
+
+
+#bps = beats per second
+bps = int(tempo) / 60#seconds
+
 
