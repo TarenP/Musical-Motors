43270f76ab94e42b5383a99780f0de97ead98095
Taren P <63258880+TarenP@users.noreply.github.com>
2021-08-04 13:35:08 -0700

    Working on threading. Getting end tick measurements right before working on sorting

--- music.py
+++ music.py
@@ -13,6 +13,12 @@ import time
 import shutil
 import csv
 import threading
+import math
+
+
+notes2 = [165, 110,  73, 69, 55, 35, 28, 21, 14]
+noteskey2 = ["dotted1/8",  "1/8", "tripletnote", "dotted1/16", "1/16", "dotted1/32", "1/32", "dotted1/64", "1/64"]
+
 
 filetype = int(input("1 for .mid filetype, 2 for .wav filetype "))
 
@@ -40,6 +46,16 @@ else:
     file_num = int(input("choose: "))
     file = files[file_num - 1]
     print(file)
+
+
+
+
+def closest(lst, K):
+    return lst[min(range(len(lst)), key = lambda i: abs(lst[i]-K))]
+
+
+
+
 try:
     os.remove("music.csv")
 except:
@@ -60,37 +76,70 @@ element.send_keys("C:\\Users\\taren\Documents\\GitHub Repositories\\Musical-Moto
 time.sleep(2)
 mid = mido.MidiFile(file)
 num = 1
-try:
-    for msg in mid.play():
-        MBT = driver.find_element(By.XPATH, '//*[@id="idTbodyMidiData"]/tr['+ str(num) + ']/td[1]').text
-        M = (MBT.split(":")[0])
-        B = (MBT.split(":")[1])
-        T = (MBT.split(":")[2])
-        #print(MBT)
-        Pitch = driver.find_element(By.XPATH, '//*[@id="idTbodyMidiData"]/tr['+ str(num) + ']/td[2]').text
-        #print(Pitch)
-        Velocity = driver.find_element(By.XPATH, '//*[@id="idTbodyMidiData"]/tr['+ str(num) + ']/td[3]').text
-        #print(Velocity)
-        Duration = driver.find_element(By.XPATH, '//*[@id="idTbodyMidiData"]/tr['+ str(num) + ']/td[4]').text
-        Duration = (MBT.split(":")[0])
-        #print(Duration)
-        tup1 = (M)
-        tup2 = (B)
-        tup3 = (T)
-        tup4 = (Pitch)
-        tup5 = (Velocity)
-        tup6 = (Duration)
-        writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+# try:
+for msg in mid.play():
+    Pitch = driver.find_element(By.XPATH, '//*[@id="idTbodyMidiData"]/tr['+ str(num) + ']/td[2]').text
+    Velocity = driver.find_element(By.XPATH, '//*[@id="idTbodyMidiData"]/tr['+ str(num) + ']/td[3]').text
+    Duration = driver.find_element(By.XPATH, '//*[@id="idTbodyMidiData"]/tr['+ str(num) + ']/td[4]').text
+    if (Duration.find(':') != -1):
+        Duration1 = (Duration.split(":")[0])
+        Duration2 = (Duration.split(":")[1])
+        if 0 < int(Duration1):
+            Note1 = int(Duration1) * 220
+        else:
+            Note1 = 0
+        
+        if 0 < int(Duration2):
+            Note2 = (closest(notes2, int(Duration2)))
+        else:
+            Note2 = 0
 
-        writer.writerow([tup1, tup2, tup3, tup4, tup5, tup6])
-        num += 1
-except:
-    print("done filling out csv")
+        Note = (Note1 + Note2)
+    else:
+        if 0 < int(Duration):
+            Note = (closest(notes2, int(Duration)))
+        else:
+            Note = 0
 
-web_tempo = driver.find_element(By.XPATH, '//*[@id="idTxtTempo"]')
-tempo = web_tempo.get_attribute("value")
-print(tempo)
-time.sleep(2)
+    MBT = driver.find_element(By.XPATH, '//*[@id="idTbodyMidiData"]/tr['+ str(num) + ']/td[1]').text
+    M = (MBT.split(":")[0])
+    B = (MBT.split(":")[1])
+    T = (MBT.split(":")[2])
+    new_MBT = int(T) + int(Note)
+    if new_MBT >= 220:
+        new_B = math.floor(new_MBT / 220 + int(B))
+        new_T = new_MBT % 220
+        if new_B > 4:
+            new_M = int(M) + 1 #Fix this, new_B isn't always 1, it needs to be able to be higher that 1. M needs to work with multiple measure changes
+            new_B = 1
+        else:
+            new_M = M
+    else:
+        new_M = int(M)
+        new_B = int(T)
+        new_T = int(T) + int(Note)
+
+    tup1 = (M)
+    tup2 = (B)
+    tup3 = (T)
+    tup4 = (Pitch)
+    tup5 = (Velocity)
+    tup6 = (Note)
+    tup7 = (new_M)
+    tup8 = (new_B)
+    tup9 = (new_T)
+    writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    writer.writerow([tup1, tup2, tup3, tup4, tup5, tup6, tup7, tup8, tup9])
+    num += 1
+    # web_tempo = driver.find_element(By.XPATH, '//*[@id="idTxtTempo"]')
+    # tempo = web_tempo.get_attribute("value")
+    # print(tempo)
+    # time.sleep(2)
+# except:
+#     print("done filling out csv")
+
+
 
 
 #used for downloading midi file, renaming it, and moving it into this folder from website
@@ -107,25 +156,58 @@ time.sleep(2)
 #bps = beats per second
 # bps = int(tempo) / 60#seconds
 
-with open('music.csv', 'r') as csv_file:
-    csv_reader = csv.reader(csv_file)
-    consecutive = [0]
-    num_threads = 0
-    for line in csv_reader:
-        if line[1] != consecutive[0]:
-            current = len(consecutive)
-            if current >= num_threads:
-                num_threads = current
-            consecutive = [line[1]]
-        else:
-            consecutive.append(line[1])
+
+
+# with open('music.csv', 'r') as csv_file:
+#     csv_reader = csv.reader(csv_file)
+#     consecutive = [0]
+#     num_threads = 0
+#     new_MBT = 0
+#     for line in csv_reader:
+        
+
+
+
+
+
+    #     if line[1] != consecutive[0]:
+    #         current = len(consecutive)
+    #         if current >= num_threads:
+    #             num_threads = current
+    #         consecutive = [line[1]]
+    #     else:
+    #         consecutive.append(line[1])
+
+    # for i in range(num_threads):
+    #     globals()[f"list{i}"] = []
+    
+
+# with open('music.csv', 'r') as csv_:
+#     reader = csv.reader(csv_)
+#     consecutiveM = [0]
+#     consecutiveB = [0]
+#     for line in reader:
+#         if line[0] == consecutiveM[0]:
+#             if line[1] == consecutiveB[0]:
+#                 globals()[f"list{len(consecutiveB)}"].append(line)
+#         else:
+#             globals()[f"list{0}"].append(line)
+#             consecutiveM = [line[0]]
+#             consecutiveB = [line[1]]
+#     print(globals()[f"list{0}"])
+#     print(globals()[f"list{1}"])
+#     print(globals()[f"list{2}"])
+#     print(globals()[f"list{3}"])
+#     print(globals()[f"list{4}"])
+#     print(globals()[f"list{5}"])
+
 
 #Create sorting algo to make sure each note in a chord has its own thread
 
-def music_thread(thread):
-    print(sdad)
+# def music_thread(thread):
+#     print(sdad)
 
-    
-for i in num_threads:
-    "T" + str(i) = threading.Thread(target=music_thread, args=("T" + str(i), "list" + str(i)))
+
+# for i in num_threads:
+#     "T" + str(i) = threading.Thread(target=music_thread, args=("T" + str(i), globals()['list%s' % i]))
 
